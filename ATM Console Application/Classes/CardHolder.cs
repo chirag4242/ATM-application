a70e4c9ea87883fc7028854e04d6b0baef46b0dd
Chirag Patil <patilchirag484@gmail.com>
2022-12-01 07:49:16 -0500

    CardHolder Class initial

---
+++ ATM Console Application/Classes/CardHolder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_Console_Application.Classes
+{
+    internal class CardHolder
+    {
+        public string cardNumber { get; set; }
+
+        public int pin { get; set; }   
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phone { get; set; }
+
+        public double accountBalance { get; set; }   
+    
+    }
+}
